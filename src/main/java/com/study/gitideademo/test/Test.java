f3326734e4e58ff59c01a2282a3dd1b823afcbc3
MuYan <XinHanser@outlook.com>
2021-06-24 19:05:26 +0800

    Version2.1

---
+++ src/main/java/com/study/gitideademo/test/Test.java
@@ -0,0 +1,11 @@
+package com.study.gitideademo.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Git idea");
+        System.out.println("Git Diy-branch");
+        System.out.println("main test");
+        System.out.println("Diy-branch tet");
+    }
+
+}
---
+++ src/test/java/com/study/gitideademo/GitIdeaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.gitideademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitIdeaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
